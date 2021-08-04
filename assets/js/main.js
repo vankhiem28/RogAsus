fb6b602c5aec73a2965939de6f4d0404c140eb47
vankhiem <vankhiem284@gmail.com>
2021-08-04 13:31:06 +0700

    add file

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,17 @@ $('.slick-slider').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     cssEase: 'linear',
-    prevArrow:'<button type="button" class="slick-prev"><i class="ti-angle-left slick-slider-icon"></i></button>',
-    nextArrow:'<button type="button" class="slick-next"><i class="ti-angle-right slick-slider-icon"></i></button>',
+    prevArrow:'<button type="button" class="slick-prev-slide-top"><i class="ti-angle-left slick-slider-icon"></i></button>',
+    nextArrow:'<button type="button" class="slick-next-slide-top"><i class="ti-angle-right slick-slider-icon"></i></button>',
     });
+
+
+
+
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        prevArrow:'<button type="button" class="slick-prev-slide-discover"><i class="ti-angle-left slick-slider-icon"></i></button>',
+        nextArrow:'<button type="button" class="slick-next-slide-discover"><i class="ti-angle-right slick-slider-icon"></i></button>',
+      }); 
