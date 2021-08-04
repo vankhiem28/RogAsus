fe47f7cc89072d417eb287ba662e9bf1a2cde2ab
vankhiem <vankhiem284@gmail.com>
2021-08-04 10:36:03 +0700

    4/8/2021 add file

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 400,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+    prevArrow:'<button type="button" class="slick-prev"><i class="ti-angle-left slick-slider-icon"></i></button>',
+    nextArrow:'<button type="button" class="slick-next"><i class="ti-angle-right slick-slider-icon"></i></button>',
+    });
